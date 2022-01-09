ba180620d7437d80c963c3f86895da896e464f31
samwinslow <sammywinslow@gmail.com>
2022-01-09 09:57:29 -0500

    env, example, form css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+function catchFormSubmit(event) {
+
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Thesaurus
       </header>
+      <form onSubmit={catchFormSubmit}>
+        <input type="text" name="sentence" placeholder="Your sentence" />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
