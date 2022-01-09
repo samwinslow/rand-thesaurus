10d3a2f67d342aa79fdca0e9812c26d32575f048
samwinslow <sammywinslow@gmail.com>
2022-01-09 11:11:09 -0500

    looks up dictionary def

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 async function getThesaurusResponse(word, apiKey) {
@@ -8,17 +8,46 @@ async function getThesaurusResponse(word, apiKey) {
   return json;
 }
 
+function isWordNotFound(response) {
+  return Array.isArray(response) && response.every(entry => typeof entry === 'string')
+}
+
+function getFirstWord(response) {
+  console.log('getting', { response })
+  if (isWordNotFound(response)) return null
+  const word = response[0]
+  const def = word.def[0].sseq[0][0][1]
+  return {
+    id: word.meta.id,
+    fl: word.fl,
+    def: {
+      sense: def.dt[0][1],
+      viz: cleanLatex(def.dt[1][1][0].t),
+    },
+  }
+}
+
+function cleanLatex(latex) {
+  return latex.replace(/\{\/?it\}/g, '')
+}
+
 function App() {
 
   const [apiKey, setApiKey] = useState('');
   const [sentence, setSentence] = useState('');
   const [responseData, setResponseData] = useState([]);
+  const [firstWord, setFirstWord] = useState(null);
 
   async function catchFormSubmit(event) {
     event.preventDefault();
     const response = await getThesaurusResponse(sentence, apiKey);
     setResponseData(response);
   }
+
+  useEffect(() => {
+    const word = getFirstWord(responseData)
+    setFirstWord(word)
+  }, [responseData])
 
   return (
     <div className="App">
@@ -45,7 +74,18 @@ function App() {
         <input type="submit" />
       </form>
       <div style={{ textAlign: 'left', padding: '0 2rem', fontSize: '150%' }}>
-        Result (JSON):
+        <div>
+          {isWordNotFound(responseData) ? (
+            <p>{sentence && <i>Word not found</i>}</p>
+          ) : (
+            <p>
+              Result <br />
+              <b>{firstWord.id} (<i>{firstWord.fl}</i>)</b>{'. '}
+              <i>{firstWord.def.sense.trim()}</i>, as in "{firstWord.def.viz}"
+            </p>
+          )}
+        </div>
+        <p>Result (JSON):</p>
         <pre>
           {JSON.stringify(responseData, null, 2)}
         </pre>
