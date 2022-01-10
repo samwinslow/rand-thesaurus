d27fa1b721ff8e029e3be7e63fce5a7027f121e7
samwinslow <sammywinslow@gmail.com>
2022-01-09 23:44:19 -0500

    history

--- src/App.js
+++ src/App.js
@@ -17,13 +17,14 @@ function getFirstWord(response) {
   if (isWordNotFound(response)) return null
   const word = response[0]
   const def = word.def[0].sseq[0][0][1]
+  console.log({ word, def })
   return {
     id: word.meta.id,
     fl: word.fl,
     def: {
       sense: cleanLatex(def.dt?.[0]?.[1]),
       viz: cleanLatex(def.dt?.[1]?.[1]?.[0]?.t),
-      syn_list: def.syn_list.flatMap(syns => syns.map(({ wd }) => wd))
+      syn_list: def.syn_list?.flatMap(syns => syns.map(({ wd }) => wd)) || []
     },
   }
 }
@@ -39,6 +40,11 @@ function App() {
   const [sentence, setSentence] = useState('');
   const [responseData, setResponseData] = useState([]);
   const [firstWord, setFirstWord] = useState(null);
+  const [history, setHistory] = useState([]); // Word-Synonym tuples
+
+  function pushToHistory(tuple) {
+    return setHistory((history) => [...history, tuple])
+  }
 
   async function catchFormSubmit(event) {
     event.preventDefault();
@@ -94,22 +100,42 @@ function App() {
                   firstWord.def.syn_list.length ? (
                     <ul>
                       {firstWord.def.syn_list.map((syn, i) => (
-                        <li key={`${i}_${syn}`}>{syn}</li>
+                        <li key={`${i}_${syn}`}>{syn} <a href="#" onClick={() => pushToHistory([firstWord.id, syn])}>+ add</a></li>
                       ))}
                     </ul>
                   ) : (
                     <p>None found.</p>
                   )
                 }
+                <ul>
+                  <li>"{firstWord.id}" (unchanged) <a href="#" onClick={() => pushToHistory([firstWord.id, firstWord.id])}>+ add</a></li>
+                </ul>
                 </div>
               </>
             )
           )}
         </div>
-        <p>Result (JSON):</p>
-        <pre>
-          {JSON.stringify(responseData, null, 2)}
+        <p>Your sentence:</p>
+        <pre style={{ fontSize: '100%' }}>
+          Original:<br />
+          {history.map(([word]) => word).join(' ')}<br /><br />
+          Thesaurified:<br />
+          {history.map(([, syn]) => syn).join(' ')}
         </pre>
+        <details>
+          <summary>
+            History <a href="#" onClick={() => setHistory([])}>clear</a>
+          </summary>
+          <pre>
+            {JSON.stringify(history, null, 2)}
+          </pre>
+        </details>
+        <details>
+          <summary>Last API response</summary>
+          <pre>
+            {JSON.stringify(responseData, null, 2)}
+          </pre>
+        </details>
       </div>
     </div>
   );
